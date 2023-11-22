d388ebeeacb7932ee71ca52a47199f7688107c11
Adam <adambaldyga@gmail.com>
2023-11-22 16:11:37 +0100

    Finish 3 tasks for bootcamp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
-from tmdb_client import get_poster_url
-
-
+from flask import Flask, render_template, request
+from tmdb_client import get_poster_url, get_movies, get_single_movie, get_single_movie_cast, capitalize_all_words
 
 
 app = Flask(__name__)
@@ -9,8 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = [i for i in range(0, 8)]
-    return render_template("homepage.html", movies=movies)
+    allowed_list = ['top_rated', 'upcoming', 'popular', 'now_playing']
+    selected_list = request.args.get('list_type', 'popular')
+
+    # Check if the selected_list is in the allowed_list, otherwise default to 'popular'
+    if selected_list not in allowed_list:
+        selected_list = 'popular'
+        incorrect_input = True
+    else:
+        incorrect_input = False
+
+    movies = get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, list=allowed_list, selected_list=selected_list, incorrect_input=incorrect_input)
 
 
 @app.context_processor
@@ -18,6 +26,18 @@ def utility_processor():
     def tmdb_image_url(path, size):
         return get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    movie = get_single_movie(movie_id)
+    cast = get_single_movie_cast(movie_id)[:12]
+    return render_template("movie_details.html", movie=movie, cast=cast)
+
+
+@app.template_filter('capitalize_all_words')
+def capitalize_all_words_filter(s):
+    return capitalize_all_words(s)
 
 
 if __name__ == "__main__":
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,9 +1,11 @@
 import requests
 
 API_KEY = 'a6f2323d004454ac7e351dbbd9392290'
-API_READ_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmYyMzIzZDAwNDQ1NGFjN2UzNTFkYmJkOTM5MjI5MCIsInN1' \
+API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmYyMzIzZDAwNDQ1NGFjN2UzNTFkYmJkOTM5MjI5MCIsInN1' \
                         'YiI6IjY1NTA5MGZlMmI5MzIwMDlmZTUzNWEzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uI' \
                         'joxfQ.26vl6tYyTUBM4uBMflWTII6SguHeJou487xGT0Hf7Bc'
+
+# src="https://dummyimage.com/300x500/000/fff.jpg"  # blank picture
 
 
 def get_popular_movies():
@@ -13,9 +15,8 @@ def get_popular_movies():
     :return:
     """
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = API_READ_ACCESS_TOKEN
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
@@ -31,6 +32,51 @@ def get_poster_url(poster_api_path, size="w342"):
     """
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+
+# def get_movies(how_many, list_type):
+#     data = get_movies_list(list_type='popular')
+#     return data["results"][:how_many]
+
+def get_movies(how_many, list_type='popular'):
+    data = get_movies_list(list_type=list_type)
+    return data["results"][:how_many]
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+
+def get_movies_list(list_type):
+    """
+    Zwraca nam pełną listę filmów z wybranej kategorii.
+
+    :return:
+    """
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def capitalize_all_words(text):
+    return ' '.join(word.capitalize() for word in text.split())
 
 
 if __name__ == "__main__":
