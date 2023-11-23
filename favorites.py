9441cb21d14f02535913abe964bc50199c2459de
Adam <adambaldyga@gmail.com>
2023-11-23 22:43:00 +0100

    Add type hints

--- favorites.py
+++ favorites.py
@@ -1,12 +1,13 @@
 import json
+from typing import List
 
 
 class Favorites:
-    def __init__(self):
+    def __init__(self) -> None:
         self.filename = "static/favorites.json"
         self.list = self._load()
 
-    def _load(self):
+    def _load(self) -> List[str]:
         try:
             with open(self.filename, "r", encoding="utf-8") as file:
                 data = json.load(file)
@@ -14,17 +15,17 @@ class Favorites:
             data = []
         return data
 
-    def save(self):
+    def save(self) -> None:
         with open(self.filename, 'w', encoding='utf-8') as file:
             json.dump(self.list, file)
 
-    def _add(self, movie_id: str):
+    def _add(self, movie_id: str) -> None:
         self.list.append(movie_id)
 
-    def _remove(self, movie_id: str):
+    def _remove(self, movie_id: str) -> None:
         self.list.remove(movie_id)
 
-    def update(self, movie_id: str):
+    def update(self, movie_id: str) -> None:
         if movie_id in self.list:
             self._remove(movie_id)
         else:
